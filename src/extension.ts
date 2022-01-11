c117acbf10bc6c162f36c2c589ac563016b9e38d
Daniel Imfeld <daniel@imfeld.dev>
2022-01-10 14:25:53 -1000

    Handle document changes in .js and .ts files

--- src/extension.ts
+++ src/extension.ts
@@ -106,7 +106,34 @@ export function activate(context: ExtensionContext) {
             window.showMessage('Svelte language server restarted.');
         }
     }
+
+    function getLS() {
+        return ls;
+    }
+
+    addDidChangeTextDocumentListener(getLS);
+
     TsPlugin.create(context);
+}
+
+function addDidChangeTextDocumentListener(getLS: () => LanguageClient) {
+    // Only Svelte file changes are automatically notified through the inbuilt LSP
+    // because the extension says it's only responsible for Svelte files.
+    // Therefore we need to set this up for TS/JS files manually.
+    workspace.onDidChangeTextDocument((evt) => {
+        if (evt.textDocument.uri.endsWith('.ts') || evt.textDocument.uri.endsWith('.js')) {
+            getLS().sendNotification('$/onDidChangeTsOrJsFile', {
+                uri: evt.textDocument.uri,
+                changes: evt.contentChanges.map((c) => ({
+                    range: {
+                        start: { line: c.range.start.line, character: c.range.start.character },
+                        end: { line: c.range.end.line, character: c.range.end.character },
+                    },
+                    text: c.text,
+                })),
+            });
+        }
+    });
 }
 
 function createLanguageServer(serverOptions: ServerOptions, clientOptions: LanguageClientOptions) {
