eb0865fbd00948f4545a979b5329934c96ee66fd
kaiuri <uriel.acioli@gmail.com>
2023-08-11 12:01:13 -0300

    wip: refactor

--- src/extension.ts
+++ src/extension.ts
@@ -1,145 +1,178 @@
+import * as coc from "coc.nvim";
 import {
-    window,
-    workspace,
-    ExtensionContext,
-    LanguageClient,
-    LanguageClientOptions,
-    ServerOptions,
-    TransportKind,
-    TextDocument,
-    RevealOutputChannelOn,
-    Uri,
-    commands,
-} from 'coc.nvim';
-import path from 'path';
-import { Position, TextDocumentPositionParams } from 'vscode-languageserver-protocol';
-import { activateTagClosing } from './html/autoClose';
-import { TsPlugin } from './tsplugin';
+  commands,
+  ExtensionContext,
+  LanguageClient,
+  LanguageClientOptions,
+  RevealOutputChannelOn,
+  ServerOptions,
+  TextDocument,
+  TransportKind,
+  Uri,
+  window,
+  workspace,
+} from "coc.nvim";
+import path from "path";
+import { Position } from "vscode-languageserver-protocol";
+import { activateTagClosing } from "./html/autoClose";
+
+import { TsPlugin } from "./tsplugin";
+
+const createLanguageClientOptions = (): LanguageClientOptions => ({
+  documentSelector: [{ scheme: "file", language: "svelte" }],
+  revealOutputChannelOn: RevealOutputChannelOn.Never,
+  synchronize: {
+    configurationSection: ["svelte", "javascript", "typescript", "prettier"],
+    fileEvents: workspace.createFileSystemWatcher(
+      "{**/*.js,**/*.ts}",
+      false,
+      false,
+      false,
+    ),
+  },
+  initializationOptions: {
+    config: coc.workspace.getConfiguration("svelte.plugin"),
+  },
+});
+class SvelteLs {
+  statusItem = coc.window.createStatusBarItem();
+  disposables: coc.Disposable[] = [];
+  constructor(public client: LanguageClient) {}
+  dispose() {
+    this.statusItem.dispose();
+  }
+}
 
 let ls: LanguageClient;
+const statusItemBuilter = (ctx: ExtensionContext) => {
+  const item = coc.window.createStatusBarItem();
+  item.text = "svelte-language-server";
+  ctx.subscriptions.push(item);
+  return item;
+};
+
 export const activate = async (context: ExtensionContext) => {
-    const runtimeConfig = workspace.getConfiguration('svelte.language-server');
+  const status = statusItemBuilter(context);
+  const runtimeConfig = workspace.getConfiguration("svelte.language-server");
+  const CLIENT_OPTIONS = createLanguageClientOptions();
 
-    const { workspaceFolders } = workspace;
-    const rootPath = Array.isArray(workspaceFolders)
-        ? Uri.parse(workspaceFolders[0].uri).fsPath
-        : undefined;
+  const { workspaceFolders } = workspace;
+  const rootPath = Array.isArray(workspaceFolders)
+    ? Uri.parse(workspaceFolders[0].uri).fsPath
+    : undefined;
 
-    const tempLsPath = runtimeConfig.get<string>('ls-path');
-    const lsPath =
-        tempLsPath && tempLsPath.trim() !== ''
-            ? path.isAbsolute(tempLsPath)
-                ? tempLsPath
-                : path.join(rootPath as string, tempLsPath)
-            : undefined;
+  const tempLsPath = runtimeConfig.get<string>("ls-path");
+  const lsPath =
+    tempLsPath && tempLsPath.trim() !== ""
+      ? path.isAbsolute(tempLsPath)
+        ? tempLsPath
+        : path.join(rootPath as string, tempLsPath)
+      : undefined;
 
-    const serverModule = require.resolve(lsPath || 'svelte-language-server/bin/server.js');
+  const serverModule = require.resolve(
+    lsPath || "svelte-language-server/bin/server.js",
+  );
 
-    // Add --experimental-modules flag for people using node 12 < version < 12.17
-    // Remove this in mid 2022
-    const runExecArgv: string[] = ['--experimental-modules'];
-    let port = runtimeConfig.get<number>('port') ?? -1;
-    if (port < 0) {
-        port = 6009;
-    } else {
-        console.log('setting port to', port);
-        runExecArgv.push(`--inspect=${port}`);
-    }
-    const debugOptions = { execArgv: ['--nolazy', '--experimental-modules', `--inspect=${port}`] };
+  // Add --experimental-modules flag for people using node 12 < version < 12.17
+  // Remove this in mid 2022
+  const runExecArgv: string[] = ["--experimental-modules"];
+  let port = runtimeConfig.get<number>("port") ?? -1;
+  if (port < 0) {
+    port = 6009;
+  } else {
+    console.log("setting port to", port);
+    runExecArgv.push(`--inspect=${port}`);
+  }
+  const debugOptions = {
+    execArgv: ["--nolazy", "--experimental-modules", `--inspect=${port}`],
+  };
 
-    const serverOptions: ServerOptions = {
-        run: {
-            module: serverModule,
-            transport: TransportKind.ipc,
-            options: { execArgv: runExecArgv },
-        },
-        debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions },
-    };
+  const serverOptions: ServerOptions = {
+    run: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: { execArgv: runExecArgv },
+    },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
 
-    const serverRuntime = runtimeConfig.get<string>('runtime');
-    if (serverRuntime) {
-        serverOptions.run.runtime = serverRuntime;
-        serverOptions.debug.runtime = serverRuntime;
-        console.log('setting server runtime to', serverRuntime);
-    }
+  const serverRuntime = runtimeConfig.get<string>("runtime");
+  if (serverRuntime) {
+    serverOptions.run.runtime = serverRuntime;
+    serverOptions.debug.runtime = serverRuntime;
+    console.log("setting server runtime to", serverRuntime);
+  }
 
-    const clientOptions: LanguageClientOptions = {
-        documentSelector: [{ scheme: 'file', language: 'svelte' }],
-        revealOutputChannelOn: RevealOutputChannelOn.Never,
-        synchronize: {
-            configurationSection: ['svelte', 'javascript', 'typescript', 'prettier'],
-            fileEvents: workspace.createFileSystemWatcher('{**/*.js,**/*.ts}', false, false, false),
-        },
-        initializationOptions: { config: workspace.getConfiguration('svelte.plugin') },
-    };
+  ls = createLanguageServer(serverOptions, CLIENT_OPTIONS);
+  await ls.start();
 
-    ls = createLanguageServer(serverOptions, clientOptions);
-    await ls.start();
-
-    ls.onReady().then(() => {
-        const tagRequestor = (document: TextDocument, position: Position) => {
-            const param: TextDocumentPositionParams = {
-                textDocument: { uri: document.uri },
-                position,
-            };
-            return ls.sendRequest<string>('html/tag', param);
-        };
-        const disposable = activateTagClosing(
-            tagRequestor,
-            { svelte: true },
-            'html.autoClosingTags',
-        );
-        context.subscriptions.push(disposable);
-        window.showInformationMessage('Svelte language server now active.');
-    });
-
-    context.subscriptions.push(
-        commands.registerCommand('svelte.restartLanguageServer', async () => {
-            await restartLS(true);
-        }),
+  ls.onReady().then(() => {
+    const disposable = activateTagClosing(
+      tagRequestor,
+      { svelte: true },
+      "html.autoClosingTags",
     );
-    async function restartLS(showNotification: boolean) {
-        if (ls) {
-            await ls.stop();
-        }
-        ls = createLanguageServer(serverOptions, clientOptions);
-        await ls.onReady();
-        if (showNotification) window.showInformationMessage('Svelte language server restarted.');
-    }
+    context.subscriptions.push(disposable);
+    window.showInformationMessage("Svelte language server now active.");
+  });
 
-    function getLS() {
-        return ls;
-    }
+  context.subscriptions.push(
+    commands.registerCommand("svelte.restartLanguageServer", async () => {
+      await restartLS(true);
+    }),
+  );
+  async function restartLS(showNotification: boolean) {
+    if (ls) await ls.stop();
+    ls = createLanguageServer(serverOptions, CLIENT_OPTIONS);
+    await ls.onReady();
+    if (showNotification)
+      window.showInformationMessage("Svelte language server restarted.");
+  }
 
-    context.subscriptions.push(addDidChangeTextDocumentListener(getLS));
-
-    TsPlugin.create(context);
+  context.subscriptions.push(
+    workspace.onDidChangeTextDocument((event) => {
+      if (!event.textDocument.uri.endsWith(".ts")) return;
+      if (!event.textDocument.uri.endsWith(".js")) return;
+      ls.sendNotification("$/onDidChangeTsOrJsFile", {
+        uri: event.textDocument.uri,
+        changes: event.contentChanges.map((changes) => ({
+          text: changes.text,
+          range: {
+            start: {
+              line: changes.range.start.line,
+              character: changes.range.start.character,
+            },
+            end: {
+              line: changes.range.end.line,
+              character: changes.range.end.character,
+            },
+          },
+        })),
+      });
+    }),
+  );
+  TsPlugin.create(context);
 };
 
 export const deactivate = async () => {
-    if (ls) await ls.stop();
+  if (ls) await ls.stop();
 };
 
-function addDidChangeTextDocumentListener(getLS: () => LanguageClient) {
-    // Only Svelte file changes are automatically notified through the inbuilt LSP
-    // because the extension says it's only responsible for Svelte files.
-    // Therefore we need to set this up for TS/JS files manually.
-    return workspace.onDidChangeTextDocument((evt) => {
-        if (evt.textDocument.uri.endsWith('.ts') || evt.textDocument.uri.endsWith('.js')) {
-            getLS().sendNotification('$/onDidChangeTsOrJsFile', {
-                uri: evt.textDocument.uri,
-                changes: evt.contentChanges.map((c) => ({
-                    range: {
-                        start: { line: c.range.start.line, character: c.range.start.character },
-                        end: { line: c.range.end.line, character: c.range.end.character },
-                    },
-                    text: c.text,
-                })),
-            });
-        }
-    });
-}
+const addEventListener =
+  (ctx: ExtensionContext) => (arg: any & coc.Disposable) => {
+    ctx.subscriptions.push(arg);
+  };
 
-function createLanguageServer(serverOptions: ServerOptions, clientOptions: LanguageClientOptions) {
-    return new LanguageClient('svelte', 'Svelte', serverOptions, clientOptions);
-}
+const tagRequestor = (document: TextDocument, position: Position) =>
+  ls.sendRequest<string>("html/tag", {
+    textDocument: { uri: document.uri },
+    position,
+  });
+const createLanguageServer = (
+  serverOptions: ServerOptions,
+  clientOptions: LanguageClientOptions,
+) => new LanguageClient("svelte", "Svelte", serverOptions, clientOptions);
--- src/html/autoClose.ts
+++ src/html/autoClose.ts
@@ -6,106 +6,104 @@
  *--------------------------------------------------------------------------------------------*/
 'use strict';
 
-import { workspace, Disposable, snippetManager } from 'coc.nvim';
+import { workspace, Disposable, snippetManager, TextDocument } from 'coc.nvim';
 
 import {
-    TextDocument,
-    Position,
-    TextDocumentContentChangeEvent,
+  Position,
+  TextDocumentContentChangeEvent,
 } from 'vscode-languageserver-protocol';
 
 export function activateTagClosing(
-    tagProvider: (document: TextDocument, position: Position) => Thenable<string>,
-    supportedLanguages: { [id: string]: boolean },
-    configName: string,
+  tagProvider: (document: TextDocument, position: Position) => Thenable<string>,
+  supportedLanguages: { [id: string]: boolean },
+  configName: string,
 ): Disposable {
-    const disposables: Disposable[] = [];
-    workspace.onDidChangeTextDocument(
-        (event) => {
-            const editor = workspace.getDocument(event.textDocument.uri);
-            if (editor) {
-                onDidChangeTextDocument(editor.textDocument, event.contentChanges);
-            }
-        },
-        null,
-        disposables,
-    );
+  const disposables: Disposable[] = [];
+  workspace.onDidChangeTextDocument(
+    (event) => {
+      const editor = workspace.getDocument(event.textDocument.uri);
+      if (editor) {
+        onDidChangeTextDocument(editor.textDocument, event.contentChanges);
+      }
+    },
+    null,
+    disposables,
+  );
 
-    let isEnabled = false;
-    updateEnabledState();
+  let isEnabled = false;
+  updateEnabledState();
 
-    let timeout: NodeJS.Timer | undefined = void 0;
+  let timeout: NodeJS.Timer | undefined = void 0;
 
-    async function updateEnabledState() {
-        isEnabled = false;
-        const editor = await workspace.document;
-        if (!editor) {
-            return;
-        }
-        const document = editor.textDocument;
-        if (!supportedLanguages[document.languageId]) {
-            return;
-        }
-        if (!workspace.getConfiguration(void 0, document.uri).get<boolean>(configName)) {
-            return;
-        }
-        isEnabled = true;
+  async function updateEnabledState() {
+    isEnabled = false;
+    const editor = await workspace.document;
+    if (!editor) {
+      return;
     }
+    const document = editor.textDocument;
+    if (!supportedLanguages[document.languageId]) {
+      return;
+    }
+    if (!workspace.getConfiguration(void 0, document.uri).get<boolean>(configName)) {
+      return;
+    }
+    isEnabled = true;
+  }
 
-    async function onDidChangeTextDocument(
-        document: TextDocument,
-        changes: readonly TextDocumentContentChangeEvent[],
+  async function onDidChangeTextDocument(
+    document: TextDocument,
+    changes: readonly TextDocumentContentChangeEvent[],
+  ) {
+    if (!isEnabled) return;
+
+    const activeEditor = await workspace.document;
+    const activeDocument = activeEditor?.textDocument;
+    if (document !== activeDocument || changes.length === 0) {
+      return;
+    }
+    if (typeof timeout !== 'undefined') {
+      clearTimeout(timeout);
+    }
+    const lastChange = changes[changes.length - 1];
+    const lastCharacter = lastChange.text[lastChange.text.length - 1];
+    if (
+      ('range' in lastChange && (lastChange.rangeLength ?? 0) > 0) ||
+      (lastCharacter !== '>' && lastCharacter !== '/')
     ) {
-        if (!isEnabled) {
-            return;
-        }
-        const activeEditor = await workspace.document;
-        const activeDocument = activeEditor?.textDocument;
-        if (document !== activeDocument || changes.length === 0) {
-            return;
-        }
-        if (typeof timeout !== 'undefined') {
-            clearTimeout(timeout);
-        }
-        const lastChange = changes[changes.length - 1];
-        const lastCharacter = lastChange.text[lastChange.text.length - 1];
-        if (
-            ('range' in lastChange && (lastChange.rangeLength ?? 0) > 0) ||
-            (lastCharacter !== '>' && lastCharacter !== '/')
-        ) {
-            return;
-        }
-        const rangeStart =
-            'range' in lastChange
-                ? lastChange.range.start
-                : Position.create(0, document.getText().length);
-        const version = document.version;
-        timeout = setTimeout(async () => {
-            const position = Position.create(
-                rangeStart.line,
-                rangeStart.character + lastChange.text.length,
-            );
-            const text = await tagProvider(document, position);
-            if (text && isEnabled) {
-                const activeEditor = await workspace.document;
-                if (activeEditor) {
-                    const activeDocument = activeEditor.textDocument;
-                    if (document === activeDocument && activeDocument.version === version) {
-                        snippetManager.insertSnippet(text, false, {
-                            start: position,
-                            end: Position.create(position.line, position.character),
-                        });
-                    }
-                }
-            }
-            timeout = void 0;
-        }, 100);
+      return;
     }
-    return {
-        dispose: () => {
-            disposables.forEach((disposable) => {
-                disposable.dispose();
+    const rangeStart =
+      'range' in lastChange
+        ? lastChange.range.start
+        : Position.create(0, document.getText().length);
+    const version = document.version;
+    timeout = setTimeout(async () => {
+      const position = Position.create(
+        rangeStart.line,
+        rangeStart.character + lastChange.text.length,
+      );
+      const text = await tagProvider(document, position);
+      if (text && isEnabled) {
+        const activeEditor = await workspace.document;
+        if (activeEditor) {
+          const activeDocument = activeEditor.textDocument;
+          if (document === activeDocument && activeDocument.version === version) {
+            snippetManager.insertSnippet(text, false, {
+              start: position,
+              end: Position.create(position.line, position.character),
             });
-        },
-    };
+          }
+        }
+      }
+      timeout = void 0;
+    }, 100);
+  }
+  return {
+    dispose: () => {
+      disposables.forEach((disposable) => {
+        disposable.dispose();
+      });
+    },
+  };
 }
